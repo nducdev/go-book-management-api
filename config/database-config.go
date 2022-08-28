a4be0b78ab04ab958b5201e0b8d1b4256f7e9529
Nguyen Duc <manhduc290607@gmail.com>
2022-08-28 16:01:57 +0700

    create dto and fix database

--- config/database-config.go
+++ config/database-config.go
@@ -16,11 +16,10 @@ func SetupDatabaseConnection() *gorm.DB {
 	}
 
 	dbUser := os.Getenv("DB_USER")
-	dbPass := os.Getenv("DB_PASS")
 	dbHost := os.Getenv("DB_HOST")
 	dbName := os.Getenv("DB_NAME")
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
+	dsn := fmt.Sprintf("%s@tcp(%s:3306)/%s?charset=utf8&parseTime=True&loc=Local", dbUser, dbHost, dbName)
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
